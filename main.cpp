f08cb49f63f9bb35033f00bf5356acd2b63ed6c2
Starinin Andrey <starinin-andrey@ya.ru>
2023-03-16 21:29:44 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
 #include <vector>
 
-#include "FileHelper.h"
+
 #include "Student.h"
 #include "Subject.h"
+
+#include "FileHelper.h"
+#include "ConsoleHelper.h"
 
 using namespace std;
 
 int main() {
     vector<Student> students = ImportFromFile("J:\\students_db\\students_db.csv");
-    for (Student student : students) {
-        cout << student.last_name << " " << student.first_name << " : " << endl;
-        for (Subject subject : student.subjects) {
-            cout << "\t" << subject.name << " : ";
-            for (int mark : subject.marks) {
-                cout << mark << " ";
-            }
-            cout << endl;
-        }
-    }
+    PrintStudents(students);
 
     return 0;
 }
